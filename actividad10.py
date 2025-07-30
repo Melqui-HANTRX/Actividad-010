5fbc282481cacd30d061ae9d9a3be9f7c4e8464a
diegobarr <d1390xd.v@gmail.com>
2025-07-30 11:04:20 -0600

    AGREGADO case y comprobacion de codigos

--- actividad10.py
+++ actividad10.py
@@ -31,5 +31,42 @@ for i in range(cantidad):
         "stock": stock,
     }
     if categoria in producto:
+        categoria[producto]+=1
+    else:
+        categoria[producto]=1
+    while True:
+        print("\n--- MENÚ DE OPCIONES ---")
+        print("1. Ver lista de productos")
+        print("2. Buscar producto por código")
+        print("3. Ver valor total del inventario")
+        print("4. Ver cantidad por categoría")
+        print("5. Salir")
 
+        opcion = input("Seleccione una opcion: ")
 
+        match opcion:
+            case "1":
+                print("---Lista de productos---")
+                for codigos1, datos in inventario.items():
+                    print(f"\nCódigo: {codigos1}")
+                    print(f"Nombre: {datos['nombre']}")
+                    print(f"Categoría: {datos['categoria']}")
+                    print(f"Talla: {datos['talla']}")
+                    print(f"Precio: Q{datos['precio']:.2f}")
+                    print(f"Stock: {datos['stock']}")
+            case "2":
+                buscar = input("Ingrese el codigo a buscar: ")
+                if buscar in producto:
+                    produto = producto[buscar]
+                    print(f"\nProducto entontrado: ")
+                    print(f"Nombre: {produto['nombre']}")
+                    print(f"Categoria: {produto['categoria']}")
+                    print(f"Talla: {produto['talla']}")
+                    print(f"Precio: Q{produto['precio']:.2f}")
+                    print(f"Stock: {produto['stock']}")
+                else:
+                    print("No se encontro el producto")
+            case "3":
+                totalprecio = 0
+                for datos in inventario.values():
+                    totalprecio += datos['precio'] * datos['stock']
